503123b69005ab6d1d7a9b4943c4a9275463e047
Dhrubajyoti Bhattacharjee <dhrubajyotibhattacharjee182@gmail.com>
2025-06-12 02:32:57 +0530

    added hover, & focus effects for the dm chat card

--- app/(root)/chats/_components/DMChatCard.tsx
+++ app/(root)/chats/_components/DMChatCard.tsx
@@ -4,6 +4,8 @@ import type { Id } from "@/convex/_generated/dataModel";
 import { Card } from "@/components/ui/card";
 import { useRouter } from "next/navigation";
 import FriendAvatar from "@/components/shared/FriendAvatar";
+import { useChat } from "@/hooks/useChat";
+import { cn } from "@/lib/utils";
 
 interface DMChatCardProps {
   id: Id<"chats">;
@@ -13,12 +15,18 @@ interface DMChatCardProps {
 
 function DMChatCard({ id, username, avatarUrl }: DMChatCardProps) {
   const router = useRouter();
+  const { isActive } = useChat();
 
   return (
     <Card
-      tabIndex={-1}
+      tabIndex={0} // makes it keyboard focusable
       onClick={() => router.push(`/chats/${id}`)}
-      className="w-full truncate p-2 cursor-pointer flex flex-row items-center gap-4"
+      className={cn(
+        "w-full truncate p-2 cursor-pointer outline-2 outline-primary/50 outline-offset-2 focus-visible:outline flex flex-row items-center gap-4",
+        isActive
+          ? "bg-primary/20"
+          : "hover:bg-primary/10 focus-within:bg-primary/10"
+      )}
     >
       <div className="truncate flex items-center gap-4">
         <FriendAvatar avatarUrl={avatarUrl} username={username} />
